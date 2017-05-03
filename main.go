e0f048525c1d1fac410cd4c7e9d862c48401b1bf
durban.zhang <durban.zhang@gmail.com>
2017-05-03 10:19:40 +0800

    add no struct type methods

--- main.go
+++ main.go
@@ -9,14 +9,27 @@ type Demo struct {
 	X, Y float64
 }
 
+type MyFloat float64
+
 func (v Demo) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (f MyFloat) Fabs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+
+	return float64(f)
 }
 
 func MethodsEx() {
 	fmt.Println("+++++++++++++++++++++MethodsEx+++++++++++++++++++++")
 	v := Demo{3, 4}
 	fmt.Println(v.Abs())
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Fabs())
 }
 
 func main() {
