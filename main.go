ea5a4ae8a903c8f8b10095e5916ca14e155baba1
durban.zhang <durban.zhang@gmail.com>
2017-05-04 21:20:22 +0800

    add ErrorsEx

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 type Demo struct {
@@ -169,6 +170,30 @@ func InterfaceStringersEx() {
 	fmt.Println(x, y)
 }
 
+type MyError struct {
+	When time.Time
+	What string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
+
+func run() error {
+	return &MyError{
+		time.Now(),
+		"It's Work",
+	}
+}
+
+func ErrorsEx() {
+	fmt.Println("+++++++++++++++++++++ErrorsEx+++++++++++++++++++++")
+	if err := run(); err != nil {
+		fmt.Println(err)
+	}
+
+}
+
 func main() {
 	MethodsEx()
 	MethodsAndPointerIndirectionEx()
@@ -177,4 +202,5 @@ func main() {
 	InterfaceEmptyEx()
 	InterfaceTypeSwitchesEx()
 	InterfaceStringersEx()
+	ErrorsEx()
 }
