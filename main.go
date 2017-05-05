dec097d0464f09e486e8c38f019f5c69de025c82
durban.zhang <durban.zhang@gmail.com>
2017-05-05 21:23:11 +0800

    add syncMutexEx,CrawlEx

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"math"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -315,6 +316,110 @@ func DefaultSelectEx() {
 	}
 }
 
+type SafeCounter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (s *SafeCounter) Inc(key string) {
+	s.mux.Lock()
+	s.v[key]++
+	s.mux.Unlock()
+}
+
+func (s *SafeCounter) Value(key string) int {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	return s.v[key]
+
+}
+
+func syncMutexEx() {
+	fmt.Println("+++++++++++++++++++++syncMutexEx+++++++++++++++++++++")
+	s := SafeCounter{v: make(map[string]int)}
+
+	for i := 0; i < 1000; i++ {
+		go s.Inc("key")
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println(s.Value("key"))
+}
+
+type Fetcher interface {
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+type fakeFetcher map[string]*fakeResult
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found %s", url)
+}
+
+var fetcher = fakeFetcher{
+	"http://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"http://golang.org/pkg/",
+			"http://golang.org/cmd/",
+		},
+	},
+	"http://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/cmd/",
+			"http://golang.org/pkg/fmt/",
+			"http://golang.org/pkg/os/",
+		},
+	},
+	"http://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+	"http://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"http://golang.org/",
+			"http://golang.org/pkg/",
+		},
+	},
+}
+
+func Crawl(url string, depth int, fetcher Fetcher) {
+	if depth < 0 {
+		return
+	}
+
+	body, urls, err := fetcher.Fetch(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher)
+	}
+	return
+}
+
+func CrawlEx() {
+	Crawl("http://golang.org/", 4, fetcher)
+}
+
 func main() {
 	MethodsEx()
 	MethodsAndPointerIndirectionEx()
@@ -331,4 +436,6 @@ func main() {
 	RangeCloseEx()
 	SelectEx()
 	DefaultSelectEx()
+	syncMutexEx()
+	CrawlEx()
 }
