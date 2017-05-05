09faa89acd40dd00483d5d006a8ee2196853d3e8
durban.zhang <durban.zhang@gmail.com>
2017-05-05 14:12:51 +0800

    update DefaultSelectEx

--- main.go
+++ main.go
@@ -297,6 +297,22 @@ func SelectEx() {
 
 func DefaultSelectEx() {
 	fmt.Println("+++++++++++++++++++++SelectEx+++++++++++++++++++++")
+
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+
+	for {
+		select {
+		case <-tick:
+			fmt.Println("Tick!")
+		case <-boom:
+			fmt.Println("Boom!")
+			return
+		default:
+			fmt.Println("   .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
 
 func main() {
